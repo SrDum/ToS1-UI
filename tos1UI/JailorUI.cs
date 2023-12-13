721e6f19c7c795f7c3337358bce6d49e5a8d1b45
SrDum <bigdumdum06@gmail.com>
2023-12-13 13:41:44 -0300

    fix jailor having jail buttons after exing a townie

--- tos1UI/JailorUI.cs
+++ tos1UI/JailorUI.cs
@@ -70,7 +70,8 @@ namespace tos1UI
             int lastTargetFresh = AddJailButton.lastTargetFresh;
             PlayPhase phase = Service.Game.Sim.info.gameInfo.Data.playPhase;
             if (Service.Game.Sim.info.roleCardObservation.Data.specialAbilityRemaining == 0 ||
-                !Service.Game.Sim.info.myDiscussionPlayer.Data.alive)
+                !Service.Game.Sim.info.myDiscussionPlayer.Data.alive ||
+                Service.Game.Sim.info.menuChoiceObservations[MenuChoiceType.SpecialAbility].Data.choices.Count ==0)
             {
                 canJail = false;
             }
