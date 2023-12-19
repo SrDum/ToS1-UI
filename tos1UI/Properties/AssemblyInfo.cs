d0541bdff1b3fcf758745da2f494c66cfe482019
SrDum <bigdumdum06@gmail.com>
2023-12-19 09:28:41 -0300

    Added Admirer UI!

--- tos1UI/Properties/AssemblyInfo.cs
+++ tos1UI/Properties/AssemblyInfo.cs
@@ -5,14 +5,6 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("tos1UI")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("tos1UI")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
